03d29e8e67b10b849b97611272a99b98f6e0fe51
mjmohona <mj.mohonajahan@gmail.com>
2024-09-30 13:36:34 +0600

    j

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  contents: [
+    './layout/*.liquid'
+  ]
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
